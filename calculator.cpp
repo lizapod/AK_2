d77ae6d160b298a972c84a011a18f9615e7edd01
Lizapod <lizapodvoiska@gmail.com>
2022-09-29 22:13:35 +0300

    changes

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,18 @@
+//podvoiska
 #include "calculator.h"
-
+//eliza
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
-
+//io-04
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
-
+//lab2
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+//last christmas i gave you my hurt but the very next day you gave it away
